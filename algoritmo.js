cc56e3ea4fe8f6d62be4f4528d285b2666e99062
Araceli <aracelicabana94@gmail.com>
2023-03-20 19:53:24 -0300

    Segungo Commit

---
+++ algoritmo.js
@@ -0,0 +1,15 @@
+document.querySelector("body").innerHTML= `
+<details>
+        <summary>Punto importante</summary>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed, rem. Repellendus ea facilis, totam itaque numquam
+        voluptate similique, saepe deleniti autem illum voluptatem, vitae unde nostrum delectus facere dicta odio.
+
+        <img height="300px" width="500px"
+            src="http://resizer.sevilla.abc.es/resizer/resizer.php?imagen=https://sevilla.abc.es/estilo/bulevarsur/wp-content/uploads/sites/14/2016/08/diccionario-maquillaje-principiantes.jpg&nuevoancho=652"
+            alt="">
+        <iframe height="400px" width="700px" src="https://www.mercadolibre.com.ar/" frameborder="0"></iframe>
+        <a href="https://www.netflix.com/ar/redeem">Netflix</a>
+
+    </details>
+
+`
